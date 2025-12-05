3a1591936ed16b9ee54c8b4c2bf484db4ab2609f
scottwhat <scottcooperaus1992@gmail.com>
2025-12-06 08:15:12 +1100

    clerk inngest db auth syncing integrations added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,22 @@
+import express from "express";
+import path from "path";
+import cors from "cors";
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
 
-import express from "express"
 import { ENV } from "./lib/env.js";
-import path from "path"
 import { connectDB } from "./lib/db.js";
-import { connect } from "http2";
+import { inngest, functions } from "./lib/inngest.js";
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const __dirname = path.resolve();
-
-console.log(process.env.PORT);
+// Middleware start
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // backend, when somebody sends a get the server respons with res.statu and a json message
 
